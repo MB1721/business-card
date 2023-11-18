fe93673ba42648fd8bfdf8b222343bcc45ffad2e
mb1721 <masonbennett1721@gmail.com>
2023-11-18 00:13:47 -0500

    Add a readme

---
+++ README.md
@@ -0,0 +1,13 @@
+![MB Logo](./src/assets/images/logo/mb-logo.svg)
+
+# Mason Bennett Applications
+
+Nice to meet you! My name is Mason Bennett.
+
+## Goals
+
+My goal is to be a self-sufficient web application developer. I want to continue my education until I know how to do everything from the design of an application up to its deployment.
+
+## My Journey
+
+I began my programming journey in October 2019. I spent my first two years inconsistently learning to code in random languages until I found a project that inspired me to take my studying seriously. It was a moderately sized project that added several new features to Mojang's Minecraft video game. Though not true coding, it taught me how to use IDEs, link files, navigate my file system, and much more! Most importantly, it taught the kind of patience I needed to spend hours pouring through documentation and bug fixing just to figure out how to get a projectile to move in a straight line. Since then, I have been dedicated to learning the ins and outs of computers. Now, I am taking on the world of web development. I am only one year into my web developer journey, but in that time I have learned front-end languages: JS, CSS, HTML and their derivatives, as well as back-end: SQL, MySQL, and Node. I have learned to set up servers, create and manage databases, develop responsive websites, and build web apps from scratch.
--- src/components/Synopsis/Synopsis.js
+++ src/components/Synopsis/Synopsis.js
@@ -15,14 +15,6 @@ export default function Synopsis() {
         <h2>My Values</h2>
         <p>When I look for a company to work with, I am searching for someone with a strong identity, a clear purpose, and the determination to see their goals through. I promise to do the utmost to help your company realize its vision.</p>
       </section>
-      {/* <section className="goals">
-        <h2>Goals</h2>
-        <p>My goal is to be a self-sufficient web application developer. I want to continue my education until I know how to do everything from the design of an application up to its deployment</p>
-      </section>
-      <section className="my-journey">
-        <h2>My Journey</h2>
-        <p>I began my programming journey in October 2019. I spent my first two years inconsistently learning to code in random languages until I found a project that inspired me to take my studying seriously. It was a moderately sized project that added several new features to Mojang's Minecraft video game. Though not true coding, it taught me how to use IDEs, link files, navigate my file system, and much more! Most importantly, it taught the kind of patience I needed to spend hours pouring through documentation and bug fixing just to figure out how to get a projectile to move in a straight line. Since then, I have been dedicated to learning the ins and outs of computers. Now, I am taking on the world of web development. I am only one year into my web developer journey, but in that time I have learned front-end languages: JS, CSS, HTML and their derivatives, as well as back-end: SQL, MySQL, and Node. I have learned to set up servers, create and manage databases, develop responsive websites, and build web apps from scratch.</p>
-      </section> */}
       <section className="skills">
         <h2>Skills</h2>
         <div className="skills_container">
