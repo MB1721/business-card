705de2d420a4c0ff509665f8f5c8f91c2bd97a40
mb1721 <masonbennett1721@gmail.com>
2023-12-13 23:57:50 -0500

    Revert "Add links"
    This reverts commit 9106449703ffe948ca1a59fc9ccb143f29f9cb46.

--- src/components/LinkBox/LinkBox.js
+++ src/components/LinkBox/LinkBox.js
@@ -4,21 +4,18 @@ import discordIco from '@images/icons/discord.svg';
 import githubIco from '@images/icons/github.svg';
 import facebookIco from '@images/icons/facebook.svg';
 
-// Separate component for rendering an icon
-const IconFig = ({ icon, link }) => (
-  <figure className="icon" key={icon}>
-    <a href={link} target="_blank">
-      <img src={icon} alt={icon} />
-    </a>
-  </figure>
-);
-
 export default function LinkBox() {
-  const icons = [
-    { icon: discordIco, link: 'https://discord.com/' },
-    { icon: githubIco, link: 'https://github.com/MB1721/business-card' },
-    { icon: facebookIco, link: 'https://www.facebook.com/masson.be.23' },
-  ].map(({ icon, link }) => <IconFig icon={icon} link={link} key={link} />);
-
-  return <footer className="link-box">{icons}</footer>;
+  return (
+    <footer className="link-box">
+      <figure className="icon">
+        <img src={discordIco}/>
+      </figure>
+      <figure className="icon">
+        <img src={githubIco}/>
+      </figure>
+      <figure className="icon">
+        <img src={facebookIco}/>
+      </figure>
+    </footer>
+  );
 }
